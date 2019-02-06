f77d5ff1f2e378211cc33bf41a9f2f931166c8aa
Jamie Pham <jtkp66@gmail.com>
2019-02-06 14:55:03 -0800

    Added List and Form component to Dashbboard component and imported into App and is rendering on screen.

---
+++ leadhunter/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Leads list</h1>
+      </div>
+    )
+  }
+}
