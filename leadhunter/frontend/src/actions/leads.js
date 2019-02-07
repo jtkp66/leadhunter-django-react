500dafe5d6885c79cb7ddaff9bb51987fb182191
Jamie Pham <jtkp66@gmail.com>
2019-02-07 09:54:16 -0800

    Added delete action cr. and reducer. Delete button deletes lead from db.

--- leadhunter/frontend/src/actions/leads.js
+++ leadhunter/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_LEADS } from './types';
+import { GET_LEADS, DELETE_LEAD } from './types';
 
 // Get leads
 export const getLeads = () => dispatch => {
@@ -11,4 +11,16 @@ export const getLeads = () => dispatch => {
                 payload: res.data
             });
         }).catch(err => console.log(err));
+}
+
+// Delete Lead 
+export const deleteLead = (id) => dispatch => {
+    axios.delete(`/api/leads/${id}`)
+        .then(res => {
+            dispatch({
+                type: DELETE_LEAD,
+                payload: id
+            });
+        })
+        .catch(err => console.log(err));
 }
--- leadhunter/frontend/src/components/leads/Leads.js
+++ leadhunter/frontend/src/components/leads/Leads.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getLeads } from '../../actions/leads';
+import { getLeads, deleteLead } from '../../actions/leads';
 
 class Leads extends Component {
   static propTypes = {
@@ -27,15 +27,22 @@ class Leads extends Component {
             </tr>
           </thead>
           <tbody>
-            { this.props.leads.map(lead => (
+            {this.props.leads.map(lead => (
               <tr key={lead.id}>
                 <td>{lead.id}</td>
                 <td>{lead.name}</td>
                 <td>{lead.email}</td>
                 <td>{lead.message}</td>
-                <td><button className="btn btn-danger">Delete</button></td>
+                <td>
+                  <button
+                    onClick={this.props.deleteLead.bind(this, lead.id)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
-            )) }
+            ))}
           </tbody>
         </table>
       </Fragment>
@@ -47,4 +54,4 @@ const mapStateToProps = state => ({
   leads: state.leads.leads
 });
 
-export default connect(mapStateToProps, { getLeads })(Leads)
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads)
