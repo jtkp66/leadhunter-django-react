da8523375b80a4e277fcd96d434108d3e1e802b9
Jamie Pham <jtkp66@gmail.com>
2019-02-11 17:53:30 -0800

    Added auth actions for user authentication.

--- leadhunter/frontend/src/reducers/auth.js
+++ leadhunter/frontend/src/reducers/auth.js
@@ -1,3 +1,9 @@
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR
+} from '../actions/types';
+
 const initialState = {
     token: localStorage.getItem('token'),
     isAuthenticated: null,
@@ -5,8 +11,29 @@ const initialState = {
     user: null
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
+        case USER_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            }
+        case AUTH_ERROR:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                user: null,
+                isAuthenticated: false,
+                isLoading: false
+            }
         default:
             return state;
     }
